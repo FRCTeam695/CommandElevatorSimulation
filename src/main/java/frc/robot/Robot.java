0b3c662ee214d11374cd391b48307221836c4f96
Christopher Dickey <cjdickey36@gmail.com>
2023-12-23 12:46:21 -0500

    Add battery voltage to dashboard

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,7 +4,9 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 /** This is a sample program to demonstrate the use of elevator simulation. */
@@ -22,6 +24,7 @@ public class Robot extends TimedRobot
   @Override
   public void robotPeriodic() {
     CommandScheduler.getInstance().run();
+    SmartDashboard.putNumber("BatteryVoltage", RobotController.getBatteryVoltage());
   }
 
   @Override
